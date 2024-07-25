0644f429831114d94b86ffb2966a7d092258372d
brelieu05 <88119024+brelieu05@users.noreply.github.com>
2024-07-25 02:09:09 -0700

    Removed localhost in cors

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ app.use(
   }),
 );
 
-app.use(
-  cors({
-    origin: `http://localhost:5173`,
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: `http://localhost:5173`,
+//     credentials: true,
+//   }),
+// );
 
 
 
